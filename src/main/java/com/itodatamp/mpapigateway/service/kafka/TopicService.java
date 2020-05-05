d4978c9564f2f836522600e722c5d72bd402bfbc
ddanijel <danijel.fon@gmail.com>
2020-05-05 09:18:52 +0200

    create TopicService status code

--- src/main/java/com/itodatamp/mpapigateway/service/kafka/TopicService.java
+++ src/main/java/com/itodatamp/mpapigateway/service/kafka/TopicService.java
@@ -5,29 +5,32 @@ import com.itodatamp.mpapigateway.config.PropertiesBean;
 import com.itodatamp.mpapigateway.dto.CreateTopicDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.java.Log;
 import okhttp3.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log
 public class TopicService {
 
     private final PropertiesBean properties;
 
     @SneakyThrows
-    public void createTopic(String topicName) {
+    public HttpStatus createTopic(String topicName) {
         OkHttpClient client = new OkHttpClient();
         CreateTopicDTO createTopicDTO = CreateTopicDTO.builder().topicName(topicName).build();
         ObjectMapper mapper = new ObjectMapper();
 
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, mapper.writeValueAsString(createTopicDTO));
+        RequestBody body = RequestBody.create(mapper.writeValueAsString(createTopicDTO), mediaType);
         Request request = new Request.Builder()
                 .url(properties.getKafkaRestProxyURL() + "/topics")
                 .post(body)
                 .addHeader("Content-Type", "application/json")
                 .build();
-
         Response response = client.newCall(request).execute();
+        return HttpStatus.resolve(response.code());
     }
 }
